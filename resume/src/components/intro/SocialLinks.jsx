5f75a60e53d8ec48d4504d4bb1f638cc8fac3e63
Ayodeji Oludiya <devayodiya@gmail.com>
2021-08-18 09:05:52 +0100

    add sociallinks

---
+++ resume/src/components/intro/SocialLinks.jsx
@@ -0,0 +1,45 @@
+import Box from '@material-ui/core/Box'
+import { makeStyles } from '@material-ui/core/styles'
+
+import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa'
+
+const useStyles = makeStyles(theme => ({
+  link: {
+    color: 'black',
+    marginRight: theme.spacing(1)
+  }
+}))
+const SocialLinks = () => {
+  const classes = useStyles()
+
+  return (
+    <Box pt={2} pb={2}>
+      <a
+        className={classes.link}
+        href='https://www.linkedin.com/in/ayodiyah'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <FaLinkedin />
+      </a>
+      <a
+        className={classes.link}
+        href='https://www.twitter.com/ayodiyah'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <FaTwitter />
+      </a>
+      <a
+        className={classes.link}
+        href='https://www.twitter.com/ayodiyah'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <FaGithub />
+      </a>
+    </Box>
+  )
+}
+
+export default SocialLinks
