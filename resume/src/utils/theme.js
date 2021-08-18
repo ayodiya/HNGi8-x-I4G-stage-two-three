801cffc0e37fc8c865f66ba103f579ff4d2db222
Ayodeji Oludiya <devayodiya@gmail.com>
2021-08-18 09:04:30 +0100

    add  theme customisation file

---
+++ resume/src/utils/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@material-ui/core/styles'
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: 'Nunito Sans'
+  },
+  palette: {
+    primary: {
+      main: '#f7e576'
+    }
+  }
+})
