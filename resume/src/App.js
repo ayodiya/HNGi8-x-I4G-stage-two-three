664c5ade1d0d8e463755c2bb544a515728d09219
Ayodeji Oludiya <devayodiya@gmail.com>
2021-08-20 21:04:48 +0100

    import aos

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,6 +1,8 @@
 import CssBaseline from '@material-ui/core/CssBaseline'
+import { useEffect } from 'react'
 import { ThemeProvider } from '@material-ui/core/styles'
 
+import AOS from 'aos'
 import Navbar from './components/navbar'
 import Intro from './components/intro'
 import About from './components/About'
@@ -9,9 +11,14 @@ import Experience from './components/Experience'
 import Education from './components/Education'
 import Portfolio from './components/Portfolio'
 import Footer from './components/Footer'
+import 'aos/dist/aos.css'
 import { theme } from './utils/theme'
 
 function App () {
+  useEffect(() => {
+    AOS.init()
+  })
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
